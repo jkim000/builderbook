91897e619d7688ca055e3d1758d4c82540b53a27
jkim000 <joshuakim514@gmail.com>
2023-08-29 13:29:01 -0700

    book 2 tabWidth linting fix

--- book/2-begin/components/Header.jsx
+++ book/2-begin/components/Header.jsx
@@ -6,17 +6,17 @@ import Grid from '@mui/material/Grid';
 import { styleToolbar } from './SharedStyles';
 
 const Header = () => (
-  <div>
-    <Toolbar style={styleToolbar}>
-      <Grid container direction="row" justifyContent="space-around" align="center">
-        <Grid item xs={12} style={{ textAlign: 'right' }}>
-          <Link href="/login">
-            <a style={{ margin: '0px 20px 0px auto' }}>Log in</a>
-          </Link>
-        </Grid>
-      </Grid>
-    </Toolbar>
-  </div>
+    <div>
+        <Toolbar style={styleToolbar}>
+            <Grid container direction="row" justifyContent="space-around" align="center">
+                <Grid item xs={12} style={{ textAlign: 'right' }}>
+                    <Link href="/login">
+                        <a style={{ margin: '0px 20px 0px auto' }}>Log in</a>
+                    </Link>
+                </Grid>
+            </Grid>
+        </Toolbar>
+    </div>
 );
 
 export default Header;
