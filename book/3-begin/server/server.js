78cedc455e3f102ebde0ac6f05245e32b51112c3
jkim000 <joshuakim514@gmail.com>
2023-09-12 21:16:29 -0700

    book 3 begin tabbing

--- book/3-begin/server/server.js
+++ book/3-begin/server/server.js
@@ -14,10 +14,10 @@ const dev = process.env.NODE_ENV !== 'production';
 const MONGO_URL = process.env.MONGO_URL_TEST;
 
 const options = {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
 };
 
 mongoose.connect(MONGO_URL, options);
@@ -30,41 +30,41 @@ const handle = app.getRequestHandler();
 
 // Nextjs's server prepared
 app.prepare().then(() => {
-  const server = express();
+    const server = express();
 
-  // confuring MongoDB session store
-  const MongoStore = mongoSessionStore(session);
+    // confuring MongoDB session store
+    const MongoStore = mongoSessionStore(session);
 
-  const sess = {
-    name: process.env.SESSION_NAME,
-    secret: process.env.SESSION_SECRET,
-    store: new MongoStore({
-      mongooseConnection: mongoose.connection,
-      ttl: 14 * 24 * 60 * 60, // save session 14 days
-    }),
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      httpOnly: true,
-      maxAge: 14 * 24 * 60 * 60 * 1000, // expires in 14 days
-      domain: 'localhost',
-    },
-  };
+    const sess = {
+        name: process.env.SESSION_NAME,
+        secret: process.env.SESSION_SECRET,
+        store: new MongoStore({
+            mongooseConnection: mongoose.connection,
+            ttl: 14 * 24 * 60 * 60, // save session 14 days
+        }),
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            httpOnly: true,
+            maxAge: 14 * 24 * 60 * 60 * 1000, // expires in 14 days
+            domain: 'localhost',
+        },
+    };
 
-  server.use(session(sess));
+    server.use(session(sess));
 
-  // this is test code, it will be removed by the end of Chapter 3
-  server.get('/', async (req, res) => {
-    req.session.foo = 'bar';
-    const user = await User.findOne({ slug: 'team-builder-book' });
-    app.render(req, res, '/', { user });
-  });
+    // this is test code, it will be removed by the end of Chapter 3
+    server.get('/', async (req, res) => {
+        req.session.foo = 'bar';
+        const user = await User.findOne({ slug: 'team-builder-book' });
+        app.render(req, res, '/', { user });
+    });
 
-  server.get('*', (req, res) => handle(req, res));
+    server.get('*', (req, res) => handle(req, res));
 
-  // starting express server
-  server.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on ${ROOT_URL}`); // eslint-disable-line no-console
-  });
+    // starting express server
+    server.listen(port, (err) => {
+        if (err) throw err;
+        console.log(`> Ready on ${ROOT_URL}`); // eslint-disable-line no-console
+    });
 });
